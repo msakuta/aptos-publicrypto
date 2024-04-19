0a504b526f87cb15eba10cbe9eb15ecf76460ae5
msakuta <masahiro.sakuta@gmail.com>
2024-04-20 00:04:34 +0900

    Make gen_account output reader and publisher accounts

--- scripts/gen_account.ts
+++ scripts/gen_account.ts
@@ -1,6 +1,25 @@
 import { Account } from "@aptos-labs/ts-sdk";
+import { promises as fsPromises } from 'node:fs';
 
-// Create two accounts, Alice and Bob
-const alice = Account.generate();
+async function main() {
+    const reader = Account.generate();
+    const publisher = Account.generate();
 
-console.log(`account: ${alice.accountAddress}`);
+    const outFile = await fsPromises.open("../.aptos/config.yaml", "w");
+    await outFile.write("---\nprofiles:\n");
+
+    async function printAccount(name, acc) {
+        await outFile.write(`  ${name}:
+    private_key: "${acc.privateKey}"
+    public_key: "${acc.publicKey}"
+    account: ${acc.accountAddress}
+    rest_url: "https://fullnode.devnet.aptoslabs.com"
+    faucet_url: "https://faucet.devnet.aptoslabs.com"
+`);
+    }
+
+    await printAccount("reader", reader);
+    await printAccount("publisher", publisher);
+}
+
+main();
